294d094d62ec080659d32ff4fc37628ac0964522
muratcanaltun <66438521+muratcanaltun@users.noreply.github.com>
2023-03-27 21:06:26 +0300

    Added the model and DB connection for stores.

---
+++ hepsibirarada/src/main/java/com/hepsibirarada/model/Store.java
@@ -0,0 +1,74 @@
+package com.hepsibirarada.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document("stores")
+public class Store {
+    @Id
+    private String id;
+
+    private String username;
+    private String email;
+    private String password;
+    private List<String> products;
+
+    public Store(String username, String email, String password) {
+        super();
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        products = new ArrayList<String>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void addProduct(String product) {
+        this.products.add(product);
+    }
+
+    public void removeProduct(String product) {
+        this.products.remove(product);
+    }
+
+    public List<String> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<String> products) {
+        this.products = products;
+    }
+}
