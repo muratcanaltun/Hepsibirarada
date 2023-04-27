76e72c083869747a7bf07c9ca55e14bc5c5483ff
BatuhanUz35 <92627339+BatuhanUz35@users.noreply.github.com>
2023-04-27 22:18:38 +0300

    a

--- web/src/components/Navbar/Navbar.js
+++ web/src/components/Navbar/Navbar.js
@@ -1,54 +1,78 @@
 import React from "react";
 import logo from "../../images/logo.jpg";
 import './Navbar.css';
-import {useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
-const MenuItems = [
-    {
-        title: 'Add Products',
-        url: '/addProduct',
-        cName: 'nav-links',
-    },
-    {
-        title: 'Register',
-        url: '/register',
-        cName: 'nav-links',
-    },
-    {
-        title: 'Login',
-        url: '/login',
-        cName: 'nav-links',
-    },
-    {
-        title: 'Delete Account',
-        url: '/deleteAccount',
-        cName: 'nav-links',
-    }
-
+const LoggedOffItems = [
+  {
+    title: 'Register',
+    url: '/register',
+    cName: 'nav-links',
+  },
+  {
+    title: 'Login',
+    url: '/login',
+    cName: 'nav-links',
+  },
 ]
 
-function Navbar (){
-      let navigate = useNavigate();
+const LoggedInItems = [
+  {
+    title: 'Add Products',
+    url: '/addProduct',
+    cName: 'nav-links',
+  },
+  {
+    title: 'Delete Account',
+    url: '/deleteAccount',
+    cName: 'nav-links',
+  }
+]
 
-      const navigateHandler = (url) => {
-          navigate(`../${url}`, {replace: true})
+const Navbar = () => {
+  const auth = localStorage.getItem('user')
+  const navigate = useNavigate();
+
+  const navigateHandler = (url) => {
+    navigate(`../${url}`, { replace: true })
+  }
+  return (
+    <div>
+      {auth ?
+        <nav className="NavbarItems">
+          <a onClick={() => { navigate('') }}><img src={logo} className="navbar-logo" alt="" /></a>
+          <ul className="nav-menu">
+            {LoggedInItems.map((item, index) => {
+              return (
+                <li>
+                  <a className={item.cName} onClick={() => { navigateHandler(item.url) }}>
+                    {item.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+        : <nav className="NavbarItems">
+          <a onClick={() => { navigate('') }}><img src={logo} className="navbar-logo" alt="" /></a>
+          <ul className="nav-menu">
+            {LoggedOffItems.map((item, index) => {
+              return (
+                <li>
+                  <a className={item.cName} onClick={() => { navigateHandler(item.url) }}>
+                    {item.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
       }
-    return (
-      <nav className="NavbarItems">
-        <a onClick={()=> {navigate('')}}><img src={logo} className="navbar-logo" alt="" /></a>
-        <ul className= "nav-menu">
-          {MenuItems.map((item, index) => {
-            return (
-              <li>
-                <a className={item.cName} onClick={() => {navigateHandler(item.url)}}>
-                  {item.title}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-    );
+    </div>
+  );
 }
 
 export default Navbar;
+
+
