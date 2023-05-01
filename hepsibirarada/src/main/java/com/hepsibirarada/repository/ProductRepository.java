0c5ef8754200c36b8ef67699d6b20b118936c873
muratcanaltun <66438521+muratcanaltun@users.noreply.github.com>
2023-05-01 17:41:38 +0300

    development for HP-141,142,143

--- hepsibirarada/src/main/java/com/hepsibirarada/repository/ProductRepository.java
+++ hepsibirarada/src/main/java/com/hepsibirarada/repository/ProductRepository.java
@@ -5,10 +5,18 @@ import org.springframework.data.mongodb.repository.DeleteQuery;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.List;
+
 public interface ProductRepository extends MongoRepository<Product, String> {
     @Query("{id:'?0'}")
     Product findByID(String id);
 
     @DeleteQuery("{id:'?0'}")
     Product deleteByID(String id);
+
+    @Query("{stopSales:false}")
+    List<Product> findAllAvailable();
+
+    @Query("{stopSales:true}")
+    List<Product> findAllSuspended();
 }
